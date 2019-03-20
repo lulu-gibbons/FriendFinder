d21deb812c53ff1d92a724b899c15a8e566cdfc0
Lacey Gibbons <lazyrider@Laceys-MacBook-Pro.local>
2019-03-19 20:56:58 -0700

    created both html files. added friends to friends.js api

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friends = [
+  {
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+  "name":"That guy",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      3,
+      2,
+      6,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+  "name":"That girl",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      5,
+      4,
+      2,
+      5,
+      4,
+      1,
+      3,
+      3,
+      2
+    ]
+  }
+]
+
+module.exports = friends;
